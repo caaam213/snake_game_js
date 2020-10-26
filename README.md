8d1f3931f0bbe6ade7437eb212f05d9e74b34e6e
caaam213 <51463522+caaam213@users.noreply.github.com>
2020-10-26 17:16:04 +0100

    Finalisation du jeu en intégrant des images

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # snake_game_js
+
+Le code ne marchera pas si vous ne l'éxécutez pas sur un serveur virtuel car j'ai utilisé du Ajax.
+Il faudra donc créer un serveur virtuel sur wamp. 
+
+En cas de problème avec le serveur, il faudra appeler la fonction "valuesWithoutJSON()" et mettre en commentaire les différents appels réalisés en AJAX.
--- snake.js
+++ snake.js
@@ -10,13 +10,28 @@ var canvas = document.getElementById("canvas");
 var d1;
 var d2;
 var delay;
-var colorSnake;
+var character = "character.png";
 var colorField;
-var colorFood;
+var food;
 const width = 25;  
 var SNAKE; 
 var FOOD;
 var multi;
+var score;
+
+function valuesWithoutJSON(){
+    d1 = 32;
+    d2 = 64;
+    delay = 200;
+    colorField = "pink";
+    food="Chicken.png";
+    SNAKE=[[0,0],[1,0]];
+    FOOD=[2,8];
+    multi = 2;
+    
+
+}
+
 
 //This function is called when I click on the "Ok" button
 buttonValidate.addEventListener("click", function(){
@@ -26,136 +41,173 @@ buttonValidate.addEventListener("click", function(){
 //Play the game
 function goToGame()
 {
-    perdu = false;
     gameOn();
 }
 
-
+//Load json file when the window is loaded
 $(document).ready(function() {
-    $.getJSON("easySnake.json", function(jd) {
+    
+    
+    var combo2 = document.getElementById("level");
+    var level = combo2.value;
+    if(level=="easy")
+    {
+        jsonFile ="./easySnake"+".json";
+        document.getElementById("body").style.backgroundColor = "lightgreen";
+    }
+    else if(level=="medium")
+    {
+        jsonFile ="./mediumSnake"+".json";
+        document.getElementById("body").style.backgroundColor = "pink";
+    }
+    else
+    {
+        jsonFile ="./hardSnake"+".json";
+        document.getElementById("body").style.backgroundColor = "lightblue";
+    }
+    $.getJSON(jsonFile, function(jd) {
         d1 = jd.dimensions[0];
         d2 = jd.dimensions[1];
         delay = jd.delay;
-        colorSnake = jd.colorSnake;
         colorField = jd.colorField;
-        colorFood = jd.colorFood;
+        food = jd.foodImage;
         SNAKE = jd.snake;
         FOOD = jd.food;
-        multi = jd.multi;
-
-        console.log(jd.delay);
-        });
+        multi = jd.multi;}); 
         $("#level").change(function(event){
-                
-            var level = combo.value;
+            var combo2 = document.getElementById("level");
+            var level = combo2.value;
             console.log(level);
             if(level=="easy")
             {
                 jsonFile ="./easySnake"+".json";
+                document.getElementById("body").style.backgroundColor = "lightgreen";
             }
             else if(level=="medium")
             {
                 jsonFile ="./mediumSnake"+".json";
+                document.getElementById("body").style.backgroundColor = "pink";
             }
             else
             {
                 jsonFile ="./hardSnake"+".json";
+                document.getElementById("body").style.backgroundColor = "lightblue";
             }
             $.getJSON(jsonFile, function(jd) {
                 d1 = jd.dimensions[0];
                 d2 = jd.dimensions[1];
                 delay = jd.delay;
-                colorSnake = jd.colorSnake;
                 colorField = jd.colorField;
-                colorFood = jd.colorFood;
+                food = jd.foodImage;
                 SNAKE = jd.snake;
                 FOOD = jd.food;
-                multi = jd.multi;
-                console.log(jd.delay);
-            });
+                multi = jd.multi;});  
+                    
+                }); 
+            
+    });
+        
+        
+            
+    
+//Load the easyJSONFile by default to prevent from some problems
+function loadDefaultJSON()
+{
+    $.getJSON("easySnake.json", function(jd) {
+        d1 = jd.dimensions[0];
+        d2 = jd.dimensions[1];
+        delay = jd.delay;
+        colorField = jd.colorField;
+        food = jd.foodImage;
+        SNAKE = jd.snake;
+        FOOD = jd.food;
+        multi = jd.multi;
+        console.log(jd.delay);
         });
-               
-});
+}
 
+//This function is called when the user load the game the second time without refreshing
 function loadJSON() {
     var combo2 = document.getElementById("level");
     var level = combo2.value;
     console.log(level);
+    
     if(level=="easy")
     {
+        document.getElementById("body").style.backgroundColor = "lightgreen";
         jsonFile ="./easySnake"+".json";
     }
     else if(level=="medium")
     {
+        document.getElementById("body").style.backgroundColor = "pink";
         jsonFile ="./mediumSnake"+".json";
     }
     else
     {
+        document.getElementById("body").style.backgroundColor = "lightblue";
         jsonFile ="./hardSnake"+".json";
     }
     $.getJSON(jsonFile, function(jd) {
         d1 = jd.dimensions[0];
         d2 = jd.dimensions[1];
         delay = jd.delay;
-        colorSnake = jd.colorSnake;
         colorField = jd.colorField;
-        colorFood = jd.colorFood;
+        food = jd.foodImage;
         SNAKE = jd.snake;
         FOOD = jd.food;
         multi = jd.multi;});
 }
 
-//Select a level on the homepage
+//Recreate the home page when the user loses.
 function goToHomePage()
 {
-    var html = '<div id="homepage" style="margin-top:0%;">'+'<h1 class="text-center display-4">SNAKE JAVASCRIPT</h1>'+
-
-    '<p class="text-center mt-3">Welcome to this Snake game created by MERAOUI Camelia</p>'+
+    var html = '<div id="homepage" style="margin-top:0%;">'+
+    '<h1 class="text-center display-4">SNAKE JAVASCRIPT</h1>'+
+    '<div class="row justify-content-center">'+
+        '<img class="col-1" src="character.png" alt=""/>'+
+        '<p class="col-5 mt-3" style="font-size:18px;">Help Charlie to be bigger by giving him some food</p>'+
+    '</div>'+
 
     '<h5 class="text-center">Choose a level</h5>'+
 
     '<div class="row mt-3">'+
-        '<select class="mx-auto form-control col-5" onchange="loadJSON()" id="level" name="level">'+
-            '<option value="easy">Easy</option>'+
+        '<select class="mx-auto form-control col-5"  onchange="loadJSON()" id="level" name="level">'+
+            '<option value="easy" selected>Easy</option>'+
             '<option value="medium">Medium</option>'+
             '<option value="hard">Hard</option>'+
         '</select>'+
     '</div>'+
 
     '<div class="row mt-3">'+
-        '<button class="btn btn-secondary mx-auto px-4" onclick="gameOn()" id="validate">Ok</button>'+
-    '</div></div><canvas id="canvas">'+'</canvas></div>';
+        '<button class="btn btn-secondary mx-auto px-4"  onclick="goToGame()" id="validate">Ok</button>'+
+    '</div>'+
+    '<div class="row">'+
+        '<iframe src="https://giphy.com/embed/tVHqp1rty3dgf9jmJt" class=" mx-auto mt-5" width="250" height="250" frameBorder="0"'+
+        'class="giphy-embed" allowFullScreen></iframe><p>'+
+            '<a href="https://giphy.com/gifs/pizza-wolke-giessen-sound-on-pizzawolke-pizza-wolke-tVHqp1rty3dgf9jmJt"></a></p>'+
+    '</div>'+
+'</div>'+
+'<canvas id="canvas"></canvas>';
+    document.getElementById("body").style.backgroundColor = "lightgreen";
     container.innerHTML = html;
-
-    $.getJSON("easySnake.json", function(jd) {
-        d1 = jd.dimensions[0];
-        d2 = jd.dimensions[1];
-        delay = jd.delay;
-        colorSnake = jd.colorSnake;
-        colorField = jd.colorField;
-        colorFood = jd.colorFood;
-        SNAKE = jd.snake;
-        FOOD = jd.food;
-        multi = jd.multi;});
+    loadDefaultJSON();
         
     
-        canvas.setAttribute(name="height", 0);
-        canvas.setAttribute(name="width", 0);
-        buttonValidate.addEventListener("click", function(){
-            goToGame();
-        });
+    canvas.setAttribute(name="height", 0);
+    canvas.setAttribute(name="width", 0);
+    buttonValidate.addEventListener("click", function(){
+        goToGame();
+    });
 }
 
 
-//TODO: ADD JSON FILE TO KEEP IN MEMORY THE BEST SCORE AND DISPLAY IT WHEN THE USER LOST ON THE ALERT
-//TODO: ADD SCORE AND FILL JSON FILE. IMPROVE CONFIGURATION IN ORDER TO BE ADAPTED WITH EVERY CONFIGURATION ON THE JSON FILE
+
 
 //Launch when the game is on
 function gameOn()
 {
-    
-
-    var score = 0;
+    document.getElementById("body").style.backgroundColor = "lightgray";
+    score = 0;
     var homePageContainer = document.getElementById('homepage');
     var canvas = document.getElementById("canvas");
     homePageContainer.remove();  
@@ -163,17 +215,13 @@ function gameOn()
     container.insertAdjacentHTML("beforeend", scoreText);
     
     //Selecting a random song
-    tabSong = ["Astronomia.mp3","Naruto.mp3","OnePiece.mp3"];
+    tabSong = ["Astronomia.mp3","Naruto.mp3","OnePiece.mp3","DemonSlayer.mp3"];
     var nbSong = getRandomInt(tabSong.length);
 
     var audio = "<audio controls autoplay loop style='display:none;'><source src='"+tabSong[nbSong]+"' type='audio/mp3'></audio>";
     container.insertAdjacentHTML("beforeend", audio);
     
     //Game configuration
-    /*
-        WORLD : 16x12 d1xd2
-        SNAKE : SNAKE[0] represents the snake position. SNAKE[1] represents its length
-     */
     
     var WORLD = new Array(d1);
 
@@ -185,7 +233,16 @@ function gameOn()
             WORLD[i][j] = 0; //Fill each element of an array by 0
         }
     }
-    
+
+    //Load food images
+    function make_base()
+    {
+        base_image = new Image();
+        base_image.src = food;
+        base_image.onload = function(){
+            context.drawImage(base_image,x1Rect+width*FOOD[0],y1Rect+width*FOOD[1],width,width);
+    }
+}
     
     for(var i=0;i<SNAKE.length;i++)
     {
@@ -196,11 +253,14 @@ function gameOn()
 
 
     /*
-        Gray Background coordinates
+        Background coordinates
     */
     var x1Rect = 200;
     var y1Rect = 50;
 
+    
+    base_image2 = new Image();
+    base_image2.src = character;
     
     canvas.setAttribute(name="height", width*d2);
     canvas.setAttribute(name="width",x1Rect+(width*d1)-50);
@@ -211,11 +271,13 @@ function gameOn()
     context.fillRect(x1Rect, y1Rect, x1Rect+d1*width, width*d2);
     SNAKE.forEach(function(oneCoordinate)
     {
-        context.fillRect(x1Rect+width*oneCoordinate[0],y1Rect+width*oneCoordinate[1],width,width);
+        base_image2.onload = function(){
+            context.drawImage(base_image,x1Rect+width*oneCoordinate[0],y1Rect+width*oneCoordinate[1],width,width);
+        }
     });
-    
-    context.fillStyle = colorFood;
-    context.fillRect(x1Rect+width*FOOD[0],y1Rect+width*FOOD[1],width,width);
+        
+    make_base();
+
 
     //End game configuration
 
@@ -309,6 +371,7 @@ function gameOn()
           WORLD[x][y] = 2;
     }
     
+    //This function verify if the snake eats the food and generate another one by calling generateFood()
     function eatFood()
     {
         
@@ -342,21 +405,18 @@ function gameOn()
             console.log(FOOD[0]+ " "+FOOD[1]);
         }
         
-        
-        
-        context.fillStyle = colorFood;
-        
-        context.fillRect(x1Rect+width*FOOD[0],y1Rect+width*FOOD[1],width,width);
+        make_base();
         
 
     }
 
+    /*Actions made when the user presses a key */
 
     function goDown()
     {
         
         context.fillStyle = colorField;
-        context.fillRect(x1Rect+width*SNAKE[0][0],y1Rect+width*SNAKE[0][1],width,50);
+        context.fillRect(x1Rect+width*SNAKE[0][0],y1Rect+width*SNAKE[0][1],width,width);
         var el = [SNAKE[SNAKE.length-1][0],SNAKE[SNAKE.length-1][1]+1];
         if(SNAKE[SNAKE.length-1][1]>0)
         {
@@ -368,18 +428,21 @@ function gameOn()
         
         SNAKE.push(el);
 
+        base_image2 = new Image();
+        base_image2.src = character;
+
         for(var i=0;i<SNAKE.length;i++)
         {
             if(SNAKE[SNAKE.length-1][1]>0)
             {
                 WORLD[SNAKE[i][0]][SNAKE[i][1]]=1;
             }
-            context.fillStyle = colorSnake;
-            context.fillRect(x1Rect+width*SNAKE[SNAKE.length-1][0],y1Rect+width*SNAKE[SNAKE.length-1][1],width,width);
+            
+            context.drawImage(base_image2,x1Rect+width*SNAKE[SNAKE.length-1][0],y1Rect+width*SNAKE[SNAKE.length-1][1],width,width);
         }
         if(lose())
         {
-            alert("perdu");
+            alert(message = "Game over ! Your score:"+score);
             goToHomePage();
             stop();
         }
@@ -392,7 +455,7 @@ function gameOn()
     {
         
         context.fillStyle = colorField;
-        context.fillRect(x1Rect+width*SNAKE[0][0],y1Rect+width*SNAKE[0][1],width,50);
+        context.fillRect(x1Rect+width*SNAKE[0][0],y1Rect+width*SNAKE[0][1],width,width);
         var el = [SNAKE[SNAKE.length-1][0],SNAKE[SNAKE.length-1][1]-1];
         if(SNAKE[SNAKE.length-1][1]>0)
         {
@@ -403,6 +466,8 @@ function gameOn()
 
         
         SNAKE.push(el);
+        base_image2 = new Image();
+        base_image2.src = character;
 
         for(var i=0;i<SNAKE.length;i++)
         {
@@ -410,12 +475,12 @@ function gameOn()
             {
                 WORLD[SNAKE[i][0]][SNAKE[i][1]]=1;
             }
-            context.fillStyle = colorSnake;
-            context.fillRect(x1Rect+width*SNAKE[SNAKE.length-1][0],y1Rect+width*SNAKE[SNAKE.length-1][1],width,width);
+            
+            context.drawImage(base_image2,x1Rect+width*SNAKE[SNAKE.length-1][0],y1Rect+width*SNAKE[SNAKE.length-1][1],width,width);
         }
         if(lose())
         {
-            alert("perdu");
+            alert(message = "Game over ! Your score:"+score);
             goToHomePage();
             stop();
         }
@@ -428,23 +493,25 @@ function gameOn()
     {
        
         context.fillStyle = colorField;
-        context.fillRect(x1Rect+width*SNAKE[0][0],y1Rect+width*SNAKE[0][1],width,50);
+        context.fillRect(x1Rect+width*SNAKE[0][0],y1Rect+width*SNAKE[0][1],width,width);
         
         var el = [SNAKE[SNAKE.length-1][0]+1,SNAKE[SNAKE.length-1][1]];
         WORLD[SNAKE[0][0]][SNAKE[0][1]]=0;
         SNAKE.shift();
         
         SNAKE.push(el);
+        base_image2 = new Image();
+        base_image2.src = character;
 
         for(var i=0;i<SNAKE.length;i++)
         {
             WORLD[SNAKE[i][0]][SNAKE[i][1]]=1;
-            context.fillStyle = colorSnake;
-            context.fillRect(x1Rect+width*SNAKE[SNAKE.length-1][0],y1Rect+width*SNAKE[SNAKE.length-1][1],width,width);
+            
+            context.drawImage(base_image2,x1Rect+width*SNAKE[SNAKE.length-1][0],y1Rect+width*SNAKE[SNAKE.length-1][1],width,width);
         }
         if(lose())
         {
-            alert("perdu");
+            alert(message = "Game over ! Your score:"+score);
             goToHomePage();
             stop();
         }
@@ -458,7 +525,7 @@ function gameOn()
     {
         
         context.fillStyle = colorField;
-        context.fillRect(x1Rect+width*SNAKE[0][0],y1Rect+width*SNAKE[0][1],width,50);
+        context.fillRect(x1Rect+width*SNAKE[0][0],y1Rect+width*SNAKE[0][1],width,width);
         
         var el = [SNAKE[SNAKE.length-1][0]-1,SNAKE[SNAKE.length-1][1]];
         if(SNAKE[0][0]!=-1)
@@ -476,15 +543,15 @@ function gameOn()
             {
                 WORLD[SNAKE[i][0]][SNAKE[i][1]]=1;
             }
-            context.fillStyle = colorSnake;
-            context.fillRect(x1Rect+width*SNAKE[SNAKE.length-1][0],y1Rect+width*SNAKE[SNAKE.length-1][1],width,width);
+            
+            context.drawImage(base_image2,x1Rect+width*SNAKE[SNAKE.length-1][0],y1Rect+width*SNAKE[SNAKE.length-1][1],width,width);
 
         }
         
         
         if(lose())
         {
-            alert("perdu");
+            alert(message = "Game over ! Your score:"+score);
             goToHomePage();
             stop();
         }
